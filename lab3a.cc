bc291a80f4516d06e78299266275693b02ca812b
Jason Wan <jasonwan@apple.com>
2017-09-26 15:56:09 -0700

    lab3a

---
+++ lab3a.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+
+bool isPrime(int n) {
+    int i;
+
+    for (i = 2; i * i < n; i++) {
+        if (n % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+main() {
+    int n;
+
+    std::cin >> n;
+
+    while (n >= 0) {
+        if (n != 0 && n != 1 && isPrime(n))
+            std::cout << n << " is a prime\n";
+        else
+            std::cout << n << " is NOT a prime\n";
+
+        std::cin >> n;
+    }    
+}
